95f804fa91031bc57aeb6606d813f2c4669b8b98
zjq <1114678178@qq.com>
2018-04-10 14:13:20 +0800

    增加了微信依赖包

--- src/main/java/com/simple/mobile/HelloController.java
+++ src/main/java/com/simple/mobile/HelloController.java
@@ -1,21 +1,13 @@
 package com.simple.mobile;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-    @Autowired
-    private GirlProperties girlProperties;
 
-    @RequestMapping(value = "/say/{id}",method = RequestMethod.GET)
-    public String toSay(@PathVariable("id") Integer id){
-//        return girlProperties.getCupSize();
-        System.out.println(id);
-        return "id = "+id;
+    @GetMapping(value = "/say")
+    public String toSay(){
+        return "hello world";
     }
 }
